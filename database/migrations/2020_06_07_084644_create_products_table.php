10f569c52043351e9e97fd74f37bebff9e6d845a
Yousss <yous.yoeunfrence@gmail.com>
2020-06-07 16:17:53 +0700

    add databaste

--- database/migrations/2020_06_07_084644_create_products_table.php
+++ database/migrations/2020_06_07_084644_create_products_table.php
@@ -15,6 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->text('detail');
+            $table->text('price');
+            $table->text('stock');
+            $table->text('discount');
             $table->timestamps();
         });
     }
